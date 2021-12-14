49a10c4adfa708e7b16573c89eda67449f4c129d
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-12-13 23:54:01 -0300

    riando UsuarioController

---
+++ SalaoBeleza.API/Controllers/UsuarioController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalaoBeleza.API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalaoBeleza.API.Controllers
+{
+    public class UsuarioController : ControllerBase
+    {
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Usuario usuario)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Usuario usuario)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ SalaoBeleza.API/Model/Servico.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalaoBeleza.API.Model
+{
+    public class Servico
+    {
+        public DateTime Horario { get; set; }
+        public TipoServico  TipoServico { get; set; }
+
+    }
+}
